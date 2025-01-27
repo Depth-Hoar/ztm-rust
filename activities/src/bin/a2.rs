0b67987b4260be3dc6a5b22c664ab5c9a099fada
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2025-01-27 06:04:12 -0700

    a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,5 +8,12 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(a:i32, b:i32) -> i32 {
+  a+b
+}
+
+fn main() {
+  let answer = add(3,2);
+  println!("{:?}", answer);
+}
 
