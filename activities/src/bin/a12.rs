262592e85c321901e654ab768c57d9ab86b7850b
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2025-02-14 06:32:13 -0700

    a12

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,4 +10,51 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum BoxColor {
+    Red,
+    Black,
+    Blue,
+}
+
+impl BoxColor {
+    fn print(&self) {
+        match self {
+            BoxColor::Red => println!("red"),
+            BoxColor::Black => println!("black"),
+            BoxColor::Blue => println!("blue"),
+        }
+    }
+}
+
+struct BoxType {
+    length: i32,
+    width: i32,
+    height: i32,
+    weight: i32,
+    color: BoxColor,
+}
+
+impl BoxType {
+    fn new_box(length: i32, width: i32, height: i32, weight: i32, color: BoxColor) -> Self {
+        Self {
+            length, 
+            width, 
+            height, 
+            weight, 
+            color
+        }
+    }
+    
+    fn print(&self) {
+        println!("length: {:?}", self.length);
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("weight: {:?}", self.weight);
+        self.color.print()
+    }
+}
+
+fn main() {
+    let small_box = BoxType::new_box(3, 2, 4, 7, BoxColor::Red);
+    small_box.print();
+}
