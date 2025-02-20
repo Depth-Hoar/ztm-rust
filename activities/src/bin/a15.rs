221bb4320b2c68760d88fcf8871204166fc7f596
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2025-02-20 06:27:21 -0700

    a15

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -11,4 +11,23 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Ticket {
+    Backstage(f32, String),
+    Standard(f32),
+    Vip(f32, String),
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(50.0, "Alice".to_owned()),
+        Ticket::Standard(20.0),
+        Ticket::Vip(100.0, "Bob".to_owned()),
+    ];
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(pice, holder) => println!("Backstage price: {:?}, Holder: {:?}", pice, holder),
+            Ticket::Standard(pice) => println!("Standard price: {:?}", pice),
+            Ticket::Vip(pice, holder) => println!("VIP price: {:?}, Holder: {:?}", pice, holder),
+        }
+    }
+}
