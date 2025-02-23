57f1bdb4406c2a2afb993b72bc8ac069a4361250
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2025-02-23 08:51:08 -0700

    a19

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,4 +13,25 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut stock = HashMap::new();
+    stock.insert("Chairs", 5);
+    stock.insert("Beds", 3);
+    stock.insert("Tables", 2);
+    stock.insert("Couches", 0);
+
+    let mut total = 0;
+
+    for (item, amount) in stock.iter() {
+        total = total + amount;
+        let stock_amount = if amount == &0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{}", amount)
+        };
+        println!("item {:?}, stock = {:?}", item, stock_amount);
+    }
+    println!("total {}", total);
+}
